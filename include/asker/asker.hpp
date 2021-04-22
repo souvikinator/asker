6d63d3b7c5153008255a3aa06b7f4f966362ebd0
Souvik <souvikat001@gmail.com>
2021-04-22 13:34:46 +0530

    added selectList function

--- include/asker/asker.hpp
+++ include/asker/asker.hpp
@@ -3,6 +3,8 @@
 #include <limits>
 #include <sstream>
 #include <typeinfo>
+#include <termios.h>
+#include <unistd.h>
 #include "../termcolor/termcolor.hpp"
 
 namespace asker
@@ -14,71 +16,73 @@ namespace asker
         const int CURSOR_TO_SOL = 1; //start of line
 
         //* to move cursor n char
-        template <uint8_t n>
-        inline std::ostream &mvUp(std::ostream &stream)
+        inline std::string mvUp(int n)
         {
-            char u[8];
-            std::sprintf(u, "\033[%dA", n);
-            stream << u;
-            return stream;
+            // char u[8];
+            return "\033[" + std::to_string(n) + "A";
         }
 
         //* to move cursor n char down
-        template <uint8_t n>
-        inline std::ostream &mvDown(std::ostream &stream)
+        inline std::string mvDown(int n)
         {
-            char d[8];
-            sprintf(d, "\033[%dB", n);
-            stream << d;
-            return stream;
+            return "\033[" + std::to_string(n) + "B";
         }
 
         //* to move cursor n char to left
-        template <uint8_t n>
-        inline std::ostream &mvleft(std::ostream &stream)
+        inline std::string mvleft(int n)
         {
-            char l[8];
-            sprintf(l, "\033[%dD", n);
-            stream << l;
-            return stream;
+            return "\033[" + std::to_string(n) + "D";
         }
 
         //* to move cursor n char to right
-        template <uint8_t n>
-        inline std::ostream &mvRight(std::ostream &stream)
+        inline std::string mvRight(int n)
         {
-            char r[8];
-            sprintf(r, "\033[%dC", n);
-            stream << r;
-            return stream;
+            // std::string r;
+            return "\033[" + std::to_string(n) + "C";
         }
 
         //* to move cursor to the extreme left
-        inline std::ostream &mvStart(std::ostream &stream)
+        inline std::string mvStart()
         {
-            stream << "\033[1000D";
-            return stream;
+            return "\033[1000D";
         }
 
         //* clear line
-        template <uint8_t mode>
-        inline std::ostream &clearLn(std::ostream &stream)
+        inline std::string clearLn(int mode)
         {
             char cl[8];
             if (mode != 1 && mode != 2 && mode != 3)
             {
                 throw "Invalid mode provided in clearLn";
             }
-            sprintf(cl, "\033[%dK", mode);
-            stream << cl;
-            return stream;
+            return "\033[" + std::to_string(mode) + "K";
         }
 
         inline void showErr(const std::string &msg)
         {
-            std::cout << termcolor::red << msg << termcolor::reset;
             // move cursor to the input line (above error)
-            std::cout << _utils::mvStart << _utils::mvUp<1> << _utils::clearLn<_utils::EOL>;
+            std::cout << termcolor::red << msg << termcolor::reset << _utils::mvUp(1) << _utils::mvStart();
+        }
+
+        inline void mvToInput(const int input_msg_length)
+        {
+            std::cout << _utils::mvRight(input_msg_length) << termcolor::blue;
+        }
+
+        //* raw mode stuff
+        //? https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html
+        struct termios orig_termios;
+        inline void rawModeOff()
+        {
+            tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+        }
+        inline void rawModeOn()
+        {
+            tcgetattr(STDIN_FILENO, &orig_termios);
+            atexit(rawModeOff);
+            struct termios raw = orig_termios;
+            raw.c_lflag &= ~(ECHO | ICANON);
+            tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
         }
     }
 
@@ -87,17 +91,20 @@ namespace asker
     inline T input(const std::string &msg, bool required = false)
     {
         std::string raw_ans;
+        const int in_msg_len = msg.length() + 3; // "? " + msg + " "
         T ans;
         std::cout << termcolor::green << "? " << termcolor::reset << termcolor::bold << msg << termcolor::blue;
         while (getline(std::cin, raw_ans) && raw_ans.length() == 0 && required)
         {
             _utils::showErr("! this is a required field");
-            std::cout << termcolor::green << "? " << termcolor::reset << termcolor::bold << msg << termcolor::blue;
+            //move cursor at the end of input msg
+            _utils::mvToInput(in_msg_len);
         }
         // clear error in the next line
-        std::cout << _utils::clearLn<_utils::EOL>;
+        std::cout << _utils::clearLn(_utils::EOL);
         std::cout << termcolor::reset;
         std::istringstream sstream(raw_ans);
+        // FIXME: add type check
         sstream >> ans;
         return ans;
     }
@@ -112,10 +119,82 @@ namespace asker
         return res;
     }
 
+    template <size_t n>
+    inline std::string selectList(const std::string &msg, const std::string (&options)[n])
+    {
+        _utils::rawModeOn();
+        char key, c1, c2;
+        std::string ans;
+        int pos = 0;
+        std::cout << termcolor::green << "? " << termcolor::blue << msg << termcolor::bright_grey << "(up and down arrow key)" << termcolor::reset << std::endl;
+        // print all options
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0)
+                std::cout << termcolor::yellow << "> " << termcolor::reset << options[i] << std::endl;
+            else
+                std::cout << "  " << options[i] << std::endl;
+            pos += 1;
+        }
+        // move cursor to first option and extreme left
+        std::cout << _utils::mvUp(pos) << _utils::mvStart();
+        pos = 0;
+        while (std::cin.get(key))
+        {
+            if (iscntrl(key))
+            { //TODO: cleanup!
+                if (key == 27)
+                {
+                    std::cin.get(c1);
+                    std::cin.get(c2);
+                    if (c1 != 91)
+                        continue;
+                    switch (c2)
+                    {
+                    case 65: //up
+                        if (pos > 0)
+                        {
+                            //change ">" to " " of current option
+                            std::cout << termcolor::reset << " " << _utils::mvStart();
+                            pos -= 1;
+                            //change " " to ">" of next option
+                            std::cout << _utils::mvUp(1) << termcolor::yellow << ">" << termcolor::reset;
+                            std::cout << _utils::mvStart();
+                        }
+                        break;
+                    case 66: //down
+                        if (pos < (n - 1))
+                        {
+                            //change ">" to " " of current option
+                            std::cout << termcolor::reset << " " << _utils::mvStart();
+                            pos += 1;
+                            //change " " to ">" of next option
+                            std::cout << _utils::mvDown(1) << termcolor::yellow << ">" << termcolor::reset;
+                            std::cout << _utils::mvStart();
+                        }
+                        break;
+                    default:
+                        break;
+                    }
+                }
+                // enter
+                if (key == 10)
+                {
+                    // FIXME:
+                    ans = options[pos];
+                    std::cout << _utils::mvDown(n - (pos + 1));
+                    // mv to end options
+                    for (int i = 0; i < n - 1; i++)
+                    {
+                        std::cout << _utils::clearLn(_utils::EOL) << _utils::mvUp(1);
+                    }
+                    std::cout << ans << std::endl;
+                    break;
+                }
+            }
+        }
+        _utils::rawModeOff();
+        return ans;
+    }
+
 }
-/*
-         & for validation 
-         std::cout << termcolor::red << ">> invalid input (y/n)" << termcolor::reset;
-         std::cout << _utils::mvStart << _utils::mvUp<1> << _utils::clearLn<_utils::EOL>;
-         std::cout << termcolor::green << "? " << termcolor::reset << termcolor::bold << msg << termcolor::reset << "  [y/n] " << termcolor::blue;
-        */
--- include/main.cpp
+++ include/main.cpp
@@ -3,14 +3,21 @@
 
 int main()
 {
-   std::cout << "test-1: confirm prompt" << std::endl;
+
+   std::cout<<"test-1: select prompt"<<std::endl;
+   std::string selection;
+   std::string options[4]={"option-1","option-2","options-3","option-4"};
+   selection=asker::selectList("select options:",options);
+   std::cout<<"user selected:"<<selection<<std::endl;
+   std::cout<<std::endl;
+   std::cout << "test-2: confirm prompt" << std::endl;
    bool exit;
    exit = asker::confirm("yo! want to exit?");
    std::cout << "wants to exit:" << exit << std::endl;
-   
-   std::cout << "test-2: input prompt" << std::endl;
-   int m;//default value
-   m=asker::input<int>("yo! enter a number:",true);
-   std::cout << "user name is:" << m << std::endl;
+   // std::cout<<std::endl;
+   // std::cout << "test-3: input prompt" << std::endl;
+   // int m;//default value
+   // m=asker::input<int>("yo! enter a number:",true);
+   // std::cout << "user name is:" << m << std::endl;
    return 0;
 }
