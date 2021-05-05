5707f828c760e9a9c552484fd9367cfb7ad0457b
ThatOneTallKid <1905222@kiit.ac.in>
2021-05-05 22:30:50 +0530

    password input

--- include/main.cpp
+++ include/main.cpp
@@ -4,7 +4,11 @@
 
 int main()
 {
-
+   char pw[MAXLen] = {0};
+   char *p = pw;
+   FILE *fp = stdin;
+   ssize_t nchar = 0;
+   
    // confirm prompt demo
    std::cout << "test-2: confirm prompt" << std::endl;
    bool exit = asker::confirm("yo! want to exit?");
@@ -24,11 +28,25 @@ int main()
    std::string m; //default value
    m = asker::input<std::string>("yo! enter name:");
    std::cout << "user entered number: " << m << std::endl;
+   std::cout << std::endl;
 
    // input demo with validator
    
    // input demo with required field
+	
+   // input demo password input 
+   std::cout << "test-4 password input" << std::endl;
+   std::cout << "Enter password: ";
+   nchar = asker::getpasswd (&p, MAXLen, '*', fp);
+   std::cout << "\nyou entered : "<< p << std::endl;
+   std::cout << std::endl;
 
+   // input demo password hidden
+   std::cout << "test-5 password input" << std::endl;
+   std::cout << "Enter password: ";
+   nchar = asker::getpasswd (&p, MAXLen, 0, fp);
+   std::cout << "\nyou entered : "<< p << std::endl;   
+   std::cout << std::endl;
 
    return 0;
 }
